580e2d8fc8434157c17c6431eeb996128f801d45
Yogeshwar Bala <yogeshwarb404@gmail.com>
2020-03-15 18:34:01 +0530

    babystep 3

--- index.js
+++ index.js
@@ -133,8 +133,8 @@ bot.on("ready", async () => {
   }
 
   if (cmd === `${prefix}time`) {
-    var nowTime = new Date;
-    var sysTime = moment.unix(nowTime.now()).tz;
+    const nowTime = Date.now();
+    var sysTime = moment.unix(nowTime).tz;
     console.log(prefix+'time was called at '+sysTime);
     // var sysZoneAbbr = sysTime.zoneAbbr();
     // return message.channel.send("Current system time: "+sysTime+" ("+sysZoneAbbr+")");
