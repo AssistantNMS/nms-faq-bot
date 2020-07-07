ebb96a93ae5b82e0b0b0240f2a672a1d26ff7200
Lachlan <vapour390@gmail.com>
2020-07-07 15:17:30 +0800

    doubt it work

--- command/lfg.js
+++ command/lfg.js
@@ -3,22 +3,26 @@ const Discord = require("discord.js");
 const channels = require('../constant/channels');
 
 const lfgPing = (message, args) => {
-    let game = args[0];
-    let gameCombined = args[0] + " " + args[1];
+    //let game = args[0];
+    //let gameCombined = args[0] + " " + args[1];
 
-    let platform = args[2];
+    //let platform = args[2];
 
     if (!args.length) {
         return message.channel.send("//ERROR: You didn't provide all the data!//");
     }
 
-    if (!args[1]) {
+    if (args.length > 2) {
+        let game = args[0];
+        let platform = args[1];
 
         message.channel.send("<@&721218924942721024> " + message.author.username + " is looking for a " + game + " group on " + platform + "!");
 
     }
 
-    else {
+    else if (args.length < 2) {
+        let gameCombined = args[0] + " " + args[1];
+        let platform = args[2];
 
         message.channel.send("<@&721218924942721024> " + message.author.username + " is looking for a " + gameCombined + " group on " + platform + "!");
 
