346841b76d341fe0ca8b8b2d52489d1b581e93a7
Lachlan <vapour390@gmail.com>
2020-07-07 23:36:40 +0800

    quick id change

--- constant/channels.js
+++ constant/channels.js
@@ -2,8 +2,7 @@
 const thisGuild = '625007826913198080';
 const appRelease = '662465837558398979';
 const faq = '686177386542137369';
-const dev = '639553928136228864';
-const gaming = '660827424195608586';
+const dev = '730084144545267732';
 
 exports.thisGuild = thisGuild;
 exports.appRelease = appRelease;
